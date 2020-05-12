7e02df642c22624b5955359e5ab457e36f4e93f4
Lisa Seelye <18159+lisa@users.noreply.github.com>
2020-05-11 20:35:57 -0400

    Check webhooks for duplicate URIs (#5)
    Signed-off-by: Lisa Seelye <lisa@users.noreply.github.com>
    
    Co-authored-by: Lisa Seelye <lisa@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
@@ -15,7 +16,12 @@ const listenHostAndPort string = "0.0.0.0:8888"
 func main() {
 	logf.SetLogger(logf.ZapLogger(true))
 	log.Info("HTTP server running at", "listen", listenHostAndPort)
+	seen := make(map[string]bool)
 	for name, hook := range webhooks.Webhooks {
+		if seen[hook().GetURI()] {
+			panic(fmt.Errorf("Duplicate webhook trying to lisen on %s", hook().GetURI()))
+		}
+		seen[name] = true
 		log.Info("Listening", "webhookName", name, "URI", hook().GetURI())
 		http.HandleFunc(hook().GetURI(), hook().HandleRequest)
 	}
