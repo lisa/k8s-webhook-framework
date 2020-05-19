3d1df056dab6eb5ee61eed6f6aca79f1f084c2ab
Lisa Seelye <18159+lisa@users.noreply.github.com>
2020-05-19 18:51:31 -0400

    Remove subscription webhook (it's gone in the port source) (#15)
    Signed-off-by: Lisa Seelye <lisa@users.noreply.github.com>
    
    Co-authored-by: Lisa Seelye <lisa@users.noreply.github.com>

--- pkg/webhooks/add_subscription_hook.go
+++
@@ -1,9 +0,0 @@
-package webhooks
-
-import (
-	"github.com/lisa/k8s-webhook-framework/pkg/webhooks/subscription"
-)
-
-func init() {
-	Register("subscription", func() Webhook { return subscription.NewWebhook() })
-}
